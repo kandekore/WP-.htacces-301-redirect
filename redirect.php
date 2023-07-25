02b1fdc08fa022c5fd7c0bb52c2a235c85cf7a26
Kandekore <darren@kandekore.net>
2023-07-25 22:13:42 +0100

    .htaccess removal

--- redirect.php
+++ redirect.php
@@ -15,67 +15,73 @@ add_action('admin_menu', 'redirect_manager_menu');
 function redirect_manager_page() {
     global $wpdb;
 
-    // Form handling
-    if (isset($_POST['old_url']) && isset($_POST['new_url'])) {
-        // Use wp_make_link_relative to get a relative URL
-        $old_path = wp_make_link_relative($_POST['old_url']);
-        $new_path = wp_make_link_relative($_POST['new_url']);
+    // Check nonce for security
+    check_admin_referer('301_redirect_manager_update');
 
-        // Add the redirect to the .htaccess file
-        insert_with_markers(get_home_path() . '.htaccess', '# 301 Redirects Manager',
-            ["Redirect 301 " . $old_path . " " . $new_path]);
+    // Add handling
+    if (isset($_POST['old_url']) && isset($_POST['new_url'])) {
+        $old_url = esc_url_raw($_POST['old_url']);
+        $new_url = esc_url_raw($_POST['new_url']);
+
+        // Get the relative paths
+        $old_path = wp_make_link_relative($old_url);
+        $new_path = wp_make_link_relative($new_url);
 
         // Save the redirect to the database
         $wpdb->insert($wpdb->prefix . 'redirect_manager',
             ['redirect' => "Redirect 301 " . $old_path . " " . $new_path]);
     }
 
-    // Removal handling
+    // Remove handling
     if (isset($_POST['remove_redirect'])) {
         // Remove from the database
-        $redirect = $wpdb->get_row($wpdb->prepare("SELECT * FROM " . $wpdb->prefix . "redirect_manager WHERE id = %d", $_POST['remove_redirect']));
         $wpdb->delete($wpdb->prefix . 'redirect_manager', ['id' => $_POST['remove_redirect']]);
-
-        // Remove from the .htaccess file
-        $htaccess = file(get_home_path() . '.htaccess');
-        $lines = array_filter($htaccess, function($line) use ($redirect) {
-            return trim($line) !== $redirect->redirect;
-        });
-        insert_with_markers(get_home_path() . '.htaccess', '# 301 Redirects Manager', $lines);
     }
 
-    // Display
+    // Add all redirects to the .htaccess file
     $redirects = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "redirect_manager");
-    ?>
-    <div class="wrap">
-        <h1>301 Redirects Manager</h1>
-        <form method="post">
-            <label for="old_url">Old URL:</label><br>
-            <input type="text" id="old_url" name="old_url" required><br>
-            <label for="new_url">New URL:</label><br>
-            <input type="text" id="new_url" name="new_url" required><br>
-            <input type="submit" value="Add Redirect">
-        </form>
-        <h2>Stored Redirects:</h2>
-        <?php foreach ($redirects as $redirect) : ?>
-            <p>
-                <?php 
-                    $parts = explode(' ', $redirect->redirect);
-                    $old_path = trim($parts[2]);
-                    $old_url = get_home_url(null, $old_path);
-                ?>
-                <a href="<?php echo esc_url($old_url); ?>" target="_blank">
-                    <?php echo esc_html($redirect->redirect); ?>
-                </a>
-                <form method="post" style="display:inline;">
-                    <input type="hidden" name="remove_redirect" value="<?php echo $redirect->id; ?>">
-                    <input type="submit" value="Remove">
-                </form>
-            </p>
-        <?php endforeach; ?>
-    </div>
-    <?php
+    $htaccess_lines = array_map(function($redirect) {
+        return $redirect->redirect;
+    }, $redirects);
+    insert_with_markers(get_home_path() . '.htaccess', '# 301 Redirects Manager', $htaccess_lines);
+
+    // Redirects list
+    $redirects = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "redirect_manager");
+    echo '<h2>Stored Redirects</h2>';
+    echo '<table class="widefat fixed" cellspacing="0">';
+    echo '<thead><tr><th>Redirects</th><th>Actions</th></tr></thead>';
+    echo '<tbody>';
+    foreach ($redirects as $redirect) {
+        echo '<tr>';
+        echo '<td>';
+        echo '<a href="' . get_home_url() . substr(explode(' ', esc_html($redirect->redirect))[1], 1) . '" target="_blank">';
+        echo esc_html($redirect->redirect);
+        echo '</a>';
+        echo '</td>';
+        echo '<td>';
+        echo '<form method="POST">';
+        wp_nonce_field('301_redirect_manager_update');
+        echo '<input type="hidden" name="remove_redirect" value="' . esc_attr($redirect->id) . '">';
+        echo '<input type="submit" value="Remove">';
+        echo '</form>';
+        echo '</td>';
+        echo '</tr>';
+    }
+    echo '</tbody>';
+    echo '</table>';
+
+    // Form to add new redirect
+    echo '<h2>Add New Redirect</h2>';
+    echo '<form method="POST">';
+    wp_nonce_field('301_redirect_manager_update');
+    echo '<label for="old_url">Old URL:</label><br>';
+    echo '<input type="text" id="old_url" name="old_url"><br>';
+    echo '<label for="new_url">New URL:</label><br>';
+    echo '<input type="text" id="new_url" name="new_url"><br>';
+    echo '<input type="submit" value="Add Redirect">';
+    echo '</form>';
 }
+
 
 // Create table for storing redirects
 function redirect_manager_install() {
